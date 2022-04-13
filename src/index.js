8ef18020140315f9f01e97e150a227a1070fe447
Webster Siu <webstersiu@fundpark.com>
2022-04-13 18:58:26 +0800

    gradient color update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import Random from "./random.js";
 // Creating the sketch itself
 const sketch = (p5) => {
-    let xspacing = 10; // Distance between each horizontal location
+    let block_width = 10; // Distance between each horizontal location
+    let block_height = 2;
+
     let w; // Width of entire wave
     let theta = 0.0; // Start angle at 0
     let amplitude = 25.0; // Height of wave
     let period = 40.0; // How many pixels before the wave repeats
     let dx; // Value for incrementing x
     let yvalues; // Using an array to store height values for the wave
-    let block_height = 2;
     let bg_color = 'white';
     let colorCounter = 5;
 
@@ -19,6 +20,9 @@ const sketch = (p5) => {
         [ '#FDF8E2','#FFDDD3','#F3BFB3','#5EA9BE','#9ACDE0','#CBE1EF' ],
         // sporty candy
         [ '#4F008E','#CD0BA9','#FF8331','#F2DD15','#2E2DE8' ],
+        [ '#ff0c08','#ff5608','#ff9408','#ffd208','#f7ff08','#b1ff08','#5eff08','#04c717' ],
+        [ '#03fff2','#03bcff','#03ddff','#0396ff','#0335ff','#0703ff','#5203ff','#9e03ff' ],
+        [ '#fffb03','#eaff03','#afff03','#70ff03','#1cff03','#03ff85','#03ffd1','#03d9ff' ],
     ];
 
 	p5.setup = () => {
@@ -26,12 +30,12 @@ const sketch = (p5) => {
 		const canvas = p5.createCanvas(400, 400);
         canvas.parent("app");
 
-        w = p5.width + xspacing;
+        w = p5.width + block_width;
 
         period = r.random_num(1, 3) * 100;
         // random PI
-        dx = (p5.PI / period) * xspacing;
-        yvalues = new Array(Math.floor(w / xspacing));
+        dx = (p5.PI / period) * block_width;
+        yvalues = new Array(Math.floor(w / block_width));
 
         calcWave();
 
@@ -56,33 +60,32 @@ const sketch = (p5) => {
     }
       
     const renderWave = () => {
-        // var startColor = p5.color(r.random_int(100, 255), r.random_int(100, 255), r.random_int(100, 255));
-        // var endColor = p5.color(r.random_int(100, 255), r.random_int(100, 255), r.random_int(100, 255));
         var cp = r.random_choice(colorPattern);
-        var startColor = p5.color(cp[0]); //p5.color(r.random_choice(r.random_choice(colorPattern)));
-
-
-        var endColor = p5.color(cp[cp.length -1]); //p5.color(r.random_choice(r.random_choice(colorPattern)));
+        var counter = 0;
+        var startColor = p5.color(cp[counter++]);
+        var endColor = p5.color(cp[counter++]);
         var rand_case = r.random_int(0, 3);
-        
+        var range = Math.floor(p5.width / cp.length / block_width);
+
         // A simple way to draw the wave with an ellipse at each location
         for (let x = 0; x < yvalues.length; x++) {
-            if (x % colorCounter == 0 && current_color) {
+            if (x % range == 0 && current_color) {
+                counter = counter < cp.length ? counter : 0;
                 startColor = current_color;
-                endColor = p5.color(r.random_choice(r.random_choice(colorPattern)));
+                endColor = p5.color(cp[counter++]);
             }
 
-            var current_color = setColor(x % colorCounter, startColor, endColor);
+            var current_color = setColor(x % range, range, startColor, endColor);
             
             var mid_pt = p5.height / 2 + yvalues[x];
             var height = pattern(rand_case, yvalues.length, x) + (block_height * 2);
 
-            setGradient(x * xspacing, (mid_pt - height) / block_height, height , current_color, p5.color(bg_color));
+            setGradient(x * block_width, (mid_pt - height) / block_height, height , current_color, p5.color(bg_color));
         }
     } 
 
-    const setColor = (x, c1, c2) => {
-        let inter = p5.map(x, 0, colorCounter, 0, 1);
+    const setColor = (x, range, c1, c2) => {
+        let inter = p5.map(x, 0, range, 0, 1);
         return p5.lerpColor(c1, c2, inter);
     }
 
@@ -92,7 +95,7 @@ const sketch = (p5) => {
             let c = p5.lerpColor(c1, c2, inter);
             p5.stroke(p5.color(255,255,255,50));
             p5.fill(c); 
-            p5.rect(x, i * block_height, xspacing, block_height);
+            p5.rect(x, i * block_height, block_width, block_height);
         }
     }
 
